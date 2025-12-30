cd7f5b0b8a6b09acdac2389352fe994dc758866e
Unto Ahti <developer.hekinav@gmail.com>
2025-12-30 19:08:43 +0200

    init

---
+++ src/app/actions/auth.ts
@@ -0,0 +1,20 @@
+import { SignupFormSchema, FormState } from '@/app/lib/definitions'
+ 
+export async function signup(state: FormState, formData: FormData) {
+  // Validate form fields
+  const validatedFields = SignupFormSchema.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+ 
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+  const { name, email, password } = validatedFields.data
+  // e.g. Hash the user's password before storing it
+  
+}
