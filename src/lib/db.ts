a90df9afd53f9c36d5c1554c9a85bab883ad1d67
HekiNav <147833782+HekiNav@users.noreply.github.com>
2026-01-09 12:05:19 +0000

    db file update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,17 @@
 import { getCloudflareContext } from '@opennextjs/cloudflare';
-import { drizzle } from 'drizzle-orm/d1';
+import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
 import * as schema from "@/db/schema"
 
 export interface Env {
   weathercam_guessr_prod: D1Database;
 }
 
+let db: DrizzleD1Database<typeof schema> | null = null
+
 export async function createDB() {
-  const { env } = await getCloudflareContext({ async: true })
-  return drizzle((env as Env).weathercam_guessr_prod, { schema })
-} 
+  if (!db) {
+    const { env } = await getCloudflareContext({ async: true })
+    db = drizzle((env as Env).weathercam_guessr_prod, { schema })
+  }
+  return db
+}
