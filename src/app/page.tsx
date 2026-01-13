a27e3b9e671f69d8f675de6089c5d7cb810460f2
Unto Ahti <developer.hekinav@gmail.com>
2026-01-13 13:06:05 +0200

    front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
-export default function Home() {
+"use server"
+import Button from "@/components/button";
+import PlaceholderImage from "@/components/placeholderimage";
+import { getCurrentUser } from "@/lib/auth";
+import Link from "next/link";
+
+export default async function Home() {
+	const user = await getCurrentUser()
 	return (
-		<div>front page</div>
+		<div className="m-4">
+			<h1 className="font-mono text-5xl text-green-600">
+				Weathercam-guessr
+			</h1>
+			<div className="flex-row flex mt-4 shrink-1">
+				<p className="max-w-150">
+					A geoguessr-like game powered by <Link className="text-green-600 underline" href="https://www.digitraffic.fi/tieliikenne/">Fintraffic's weather camera API. </Link> Play on your own, or with friends, or even without an account*.
+					<br />
+					<small>*Practice only</small>
+				</p>
+				<PlaceholderImage className="rounded-3xl h-60 grow-2"></PlaceholderImage>	
+			</div>
+
+			<div className="flex flex-row mt-4 text-xl font-medium">
+				{user ? (
+					<div>
+						<Link href="/play"><Button>Play</Button></Link> or <Link href="/user/me"><Button>View profile</Button></Link>
+					</div>
+				) : (
+					<div>
+						<Link href="/play/practice"><Button>Practice without account</Button></Link> or <Link href="/play/practice"><Button>Log in</Button></Link>
+					</div>
+				)}
+			</div>
+		</div>
 	);
 }
--- src/components/placeholderimage.tsx
+++ src/components/placeholderimage.tsx
@@ -1,7 +1,8 @@
 import { rib } from "@/lib/definitions";
+import { HTMLAttributes } from "react";
 
-export default function PlaceholderImage() {
-    return (<div className="h-30" style={{
+export default function PlaceholderImage(props: HTMLAttributes<HTMLDivElement>) {
+    return (<div {...props} className={`h-30 ${props.className}`} style={{
         backgroundImage: "url(/background-tile-green-inverted.png)",
         backgroundSize: "10em",
         backgroundPosition: `${rib(1, 100)}em ${rib(1, 100)}em`
